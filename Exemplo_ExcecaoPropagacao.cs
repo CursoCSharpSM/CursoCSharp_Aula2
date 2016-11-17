78dbcd4d3d94731fa4d647e2235b34c6cf79803b
CursoCSharpSM <cursocsharpsm@gmail.com>
2016-11-17 19:22:02 -0200

    Update Exemplo_ExcecaoPropagacao.cs

--- Exemplo_ExcecaoPropagacao.cs
+++ Exemplo_ExcecaoPropagacao.cs
@@ -8,6 +8,9 @@ class Exemplo_ExcecaoPropagacao
 {
     static void Main(string[] args)
     {
+
+        // Todos os metodos e funcoes executados dentro deste bloco TRY
+        // serão capturados e tratados neste mesmo bloco de catch
         try
         {
             ExemploDivisao1(1, 0);
@@ -20,6 +23,7 @@ class Exemplo_ExcecaoPropagacao
 
     }
 
+    // Ou seja, os erros que acontecem neste metodo, serão tratadas no método que o chamou (Main)
     static double ExemploDivisao1(int vlr1, int vlr2)
     {
         return  vlr1 / vlr2;
